e3743e74810afb62500c148c88af82c0584c594a
DenGrif <grifon991@gmail.com>
2024-11-12 14:52:11 +0400

    add_db_01

--- app/backend/db.py
+++ app/backend/db.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import Column, Integer, String
 
 engine = create_engine("sqlite:///ecommerce.db", echo = True)
 
@@ -7,3 +8,13 @@ SessionLocal = sessionmaker(bind=engine)
 
 class Base(DeclarativeBase):
     pass
+
+
+
+
+# class User(Base):
+#     __tablename__ = "user"
+#
+#     id = Column(Integer, primary_key= True)
+#     username = Column(String)
+#     password = Column(String)
